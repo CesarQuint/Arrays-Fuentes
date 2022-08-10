dd606d3d460bbc8f635f0040813aa3047a14f6b7
CesarQuint <pedrocesarquintero001@gmail.com>
2022-08-09 20:38:58 -0500

    Proyecto 1.0

--- arrays.js
+++ arrays.js
@@ -36,34 +36,58 @@ const eleccion=()=>{
     if(respuesta==='Yes'){
         agregar();
     }else{
-       console.log('Gracias por utilizarnos');
+       console.log('Gracias por usarnos');
     }
 
 }
 
 function agregar(){
-    nameTask=prompt('Ingresa el nombre de la tarea:');
-    relev=prompt('¿Cual es la relevancia del problema?');
-    description=prompt('Ingrese la descripcion de su tarea:');
+    const nameTask=prompt('Ingresa el nombre de la tarea:');
+    const relev=Number(prompt('¿Cual es la relevancia de la tarea : 1.Alta,2.Media o 3.Baja(Ingresa el numero)?'));
+    const description=prompt('Ingrese la descripcion de su tarea:');
     const task=new toDo(nameTask,relev,description);
     listTask.push(task);
-    imprimirLista(listTask);
-    eleccion();
+    imprimirLista(listTask, relev);
+    
 }
 
-const imprimirLista= listTask=>{
+const imprimirLista= (listTask)=>{
+    
+        limpiarlista(listTask);
+       
         listTask.forEach(task => {
             numTarea=listTask.indexOf(task)+1;
             const{nameTask,relevan,description}=task;
             console.log('-------------------');
             console.log(`Numero de tarea: ${numTarea}`);
             console.log(`Nombre de la tarea: ${nameTask}`);
-            console.log(`Relevancia de la tarea: ${relevan}`);
+            switch (relevan) {
+                case 1:
+                    console.log(`Relevancia de la tarea: AlTA`);
+                    console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+                    break;
+                case 2:
+                    console.log(`Relevancia de la tarea: Media`);
+                    console.log('*****************************');
+                    break;
+                case 3:
+                    console.log(`Relevancia de la tarea: Baja`);
+                    console.log('____________________________');
+                    break;
+                default:
+                    console.log(`Relevancia de la tarea: No ingresada`);
+                    console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+                    break;
+            }
             console.log(`Descripcion de la tarea: ${description}`);
             console.log('-------------------');
         });
-    
-    
+        eleccion();
 }
 
+function limpiarlista(listTask){
+    if(listTask.length>1){
+        console.clear();
+    }
+}
    
